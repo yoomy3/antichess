6fa360ea6952d7665a89e4074fa6a045c1c3191b
Minyoung Yoo <yoomy1203@hotmail.com>
2016-11-23 20:48:46 -0500

    board and piece raw code

--- board.java
+++ board.java
@@ -1,5 +1,18 @@
 public class Board {
 	// private variables
+	Piece[8][8] mainBoard;
+	int player; // 0 - black, 1 - white
+	boolean forcedMove;
+	
+	boolean isCaptured;
+	int fiftyMoveCount;
+
+	boolean isCastled;
+
+	// constructor
+	Board() {
+		// initialize 32 pieces
+	}
 
 	// make a move, update the board
 	void makeMove(Board board) {}
